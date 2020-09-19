ddae0bf868ac658e46820caaeae9cfe28be360b5
Daniel Brendel <dbrendel1988@gmail.com>
2020-09-19 02:55:04 +0200

    This one too

--- app/InstallerModel.php
+++ app/InstallerModel.php
@@ -52,7 +52,7 @@ class InstallerModel extends Model
             $envcontent .= 'APP_TITLE="${APP_PROJECTNAME} - ${APP_DESCRIPTION}"' . PHP_EOL;
             $envcontent .= 'APP_DIVISION=' . (strlen($attr['project']) / 2) . PHP_EOL;
 			$envcontent .= 'APP_LANG=en' . PHP_EOL;
-            $envcontent .= 'APP_POSTPACKLIMIT=25' . PHP_EOL;
+            $envcontent .= 'APP_POSTPACKLIMIT=20' . PHP_EOL;
             $envcontent .= 'APP_THREADPACKLIMIT=25' . PHP_EOL;
             $envcontent .= 'APP_PUSHPACKLIMIT=25' . PHP_EOL;
 			$envcontent .= 'APP_MESSAGEPACKLIMIT=15' . PHP_EOL;
